8f1bf23d287a261f41b9a443dcde2437f978b5c9
rjba <117646976+YorNitsuga@users.noreply.github.com>
2024-07-08 11:45:38 +0800

    Add the arithmetic functions and clear button functionality

--- main.js
+++ main.js
@@ -1,1 +1,28 @@
-console.log('hi')
+function add(a, b) {
+    return a + b;
+}
+function subtract(a, b) {
+    return a - b;
+}
+function multiply(a, b) {
+    return a * b;
+}
+function divide(a, b) {
+    return a / b;
+}
+
+let prevScreen = document.querySelector('.previous');
+let currScreen = document.querySelector('.current');
+
+function clearDisplay() {
+    prevScreen.textContent = '';
+    currScreen.textContent = '';
+}
+
+let firstNumber;
+let operator;
+let secondNumber;
+
+function operate(firstNumber, operator, secondNumber) {
+    
+}
