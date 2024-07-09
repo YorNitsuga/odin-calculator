fe6ca99dc75036a45ddccc547619f280f4c27a29
rjba <117646976+YorNitsuga@users.noreply.github.com>
2024-07-09 09:28:12 +0800

    Add functionality in the clear button

--- main.js
+++ main.js
@@ -21,7 +21,15 @@ operators.forEach((op) => op.addEventListener("click", function(e) {
     handleOperator(e.target.textContent)
     previousScreen.textContent = previousValue + " " + operator;
     currentScreen.textContent = currentValue;
-} ))
+}));
+
+clear.addEventListener("click", function() {
+    previousValue = '';
+    currentValue = '';
+    operator = '';
+    previousScreen.textContent = currentValue;
+    currentScreen.textContent = currentValue;
+})
 
 function handleNumber(num) {
     if (currentValue.length <= 10) {
